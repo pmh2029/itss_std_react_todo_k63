6cf5e3ba5c33ba1c09e9292bc83bc3142ae7abcf
pmh <hieu.pm2029@gmail.com>
2022-06-06 03:18:34 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,14 @@ import useStorage from '../hooks/storage';
 import { getKey } from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-    /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
 
   const [filter, setFilter] = React.useState('ALL');
 
@@ -49,9 +50,8 @@ function Todo() {
   const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
-
+  
   const handleFilterChange = value => setFilter(value);
-
   return (
     <div className="panel">
       <div className="panel-heading">
@@ -71,6 +71,11 @@ function Todo() {
       ))}
       <div className="panel-block">
         {displayItems.length} items
+      </div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
     </div>
   );
